92748975e818c0312f597182a8657fa574873b7e
Joe Leinsdorf <leinsdorf@gmail.com>
2017-11-02 11:55:21 -0400

    intermediate commit of bust, soft ace toggle and dealer coninuation logic -- need to test

--- js/game/blackjack/Deal.js
+++ js/game/blackjack/Deal.js
@@ -4,6 +4,8 @@ import { PLAYER, DEALER } from './constants'
 
 let gameDeck
 
+const BUST_LIMIT = 21
+
 const clearHand = () => {
 
   return {
@@ -24,21 +26,73 @@ let HAND = {
 
 }
 
+
+const updateHand = participant => {
+
+  let totalVal = 0 
+
+  let aces = 0
+
+  for (let card of HAND[participant].cards) {
+    //accumulate value of hand
+    totalVal += card.value
+    //hand is soft with aces, so tally them 
+    if (card.face && card.face === 'A') {
+
+      aces++
+       
+    }
+
+  }
+
+  HAND[participant].cumulativeVal = totalVal
+
+  HAND[participant].aces = aces
+  
+}
+
+
 const dealCard = participant => {
   //select card, remove it from deck
   const card = gameDeck.splice(0, 1)[0]
   //add card to hand
   HAND[participant].cards.push(card)
-  //accumulate value of hand
-  HAND[participant].cumulativeVal += card.value
-  //if hand contains an Ace, set 'soft' prop 
-  if (card.face && card.face === 'A') {
 
-    HAND[participant].soft = true
-
-  }
+  updateHand(participant)
 
   return card
+
+}
+
+
+const handBusts = participant => {
+
+  //add soft logic//
+
+  if (HAND[participant].cumulativeVal > BUST_LIMIT) {
+
+    if (HAND[participant].aces) {
+
+      toggleSoftAce(participant)
+
+      return false
+
+    }
+
+    endRound()
+
+    return true
+
+  } 
+
+  return false
+
+}
+
+
+const blackjack = participant => {
+
+  return HAND[participant].cumulativeVal === BUST_LIMIT
 
 }
 
@@ -48,10 +102,8 @@ const dealCardToPlayer = () => {
   const card = dealCard(PLAYER)
 
   UIDeal.updateHand(PLAYER, card)
-
-  HAND[PLAYER].cumulativeVal === 21 && dealCardToDealer()
-
-  HAND[PLAYER].cumulativeVal > 21 && endRound()
+  
+  handBusts(PLAYER) || blackjack(PLAYER) && dealCardToDealer()
 
 }
 
@@ -62,21 +114,82 @@ const dealCardToDealer = () => {
   //show all cards except dealer's first
   HAND[DEALER].cards.length > 1 && UIDeal.updateHand(DEALER, card)
 
-  dealerContinues() && dealCardToDealer()
+  handBusts(DEALER) || dealerContinues() && dealCardToDealer()
 
 }
 
-const dealerContinues = () => {
+
+const toggleSoftAce = participant => {
+
+  const newHand = []
+
+  let aceToggled = false
+
+  for (let card of HAND[participant].cards) {
+
+    if (!aceToggled && card.face && card.face === 'A' && card.value === 11) {
+
+      card.value = 1
+
+      aceToggled = true
+
+    }
+
+    newHand.push(card)
+
+  }
+
+  updateHand(participant)
+
+}
+
+
+const dealerContinuesOn17 = () => {
+
+  if (HAND[DEALER].aces) {
+
+    toggleSoftAce(DEALER)
+
+    return true
+
+  }
 
   return false
 
 }
 
-const endRound = () => {
 
+const dealerContinues = () => {
 
+  const handVal = HAND[DEALER].cumulativeVal
+
+  if (blackjack(DEALER)) {
+
+    //game ends, dealer wins
+
+    return false
+
+  } else if (handVal > 17 && handVal < BUST_LIMIT) {
+
+    //dealer stands, determineWinner...
+
+    return false
+
+  } else if (handVal === 17) {
+
+    return dealerContinuesOn17()
+
+  }
+
+  return true
 
 }
+
+// const endRound = () => {
+
+
+
+// }
 
 
 export const dealGame = deck => {
